1301bf68ed5a7812d872ad6ded76b0a1a7a02226
Evan Suong <evansuong@gmail.com>
2021-02-21 03:27:26 -0800

    routes up

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("firebase/storage");
 
 const express = require("express");
 var app = express(); // Express();
+app.use(express.json());
 const port = 3000;
 const fetch = require("node-fetch");
 const server = "localhost:3000";
@@ -11,7 +12,10 @@ const server = "localhost:3000";
 const Reviews = require("./model/Reviews.js");
 const Users = require("./model/Users.js");
 
+
 const users = require("./routes/usersRoutes");
+const reviews = require("./routes/reviewsRoutes");
+const places = require("./routes/placesRoutes");
 
 // GET , PUT, POST, DELETE
 function listNamesIds(data) {
@@ -78,10 +82,13 @@ const password = "1234";
 // app.get("/")
 
 app.use("/users", users)
+app.use("/reviews", reviews)
+app.use("/places", places)
 
 app.get(`${server}/users/login/${username},${password}`, (req, res) => {
     // res.send("Hello World");
+    console.log('hi')
     console.log(req.json());
 });
 
-app.listen(port, () => console.log("listening on port" + port));
+app.listen(port, () => console.log("listening on port " + port));
--- routes/placesRoutes.js
+++ routes/placesRoutes.js
@@ -1,5 +1,26 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
+const cors = require("cors");
 
-app.get();
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+router.use(express.json());
+router.use(cors());
+
+router.get("/:city", async (req, res) => {
+  console.log("city: ", req.params.city);
+
+  // let places = get places function
+  try {
+    res.status(200).json(places);
+  } catch (err) {
+    res.status(400).send(`An error occurred: ${err}`);
+  }
+});
+
+
+
+module.exports = router;
